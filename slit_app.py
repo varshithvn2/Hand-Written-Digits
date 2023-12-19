3eff8346d6c3f3a31b881eca2056f3668e8d94ee
Varshith <75238274+varshithvn2@users.noreply.github.com>
2023-12-19 18:27:06 +0530

    Add files via upload

--- slit_app.py
+++ slit_app.py
@@ -4,12 +4,12 @@ import numpy as np
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
-# Load the trained model
-model = tf.keras.models.load_model('handwritten.model')
 
+# Load the trained model
+model = tf.keras.models.load_model('mnist_cnn_model.h5')
 # Streamlit app
-st.markdown("<h1 style='text-align: center; color: black;'>Hand Written Digit Recognition</h1>", unsafe_allow_html=True)
-st.write("Fill the canvas, draw in the center")
+st.title("Digit Recognition App")
+st.write("Draw in center")
 
 # Specify canvas parameters in the application
 drawing_mode = "freedraw"  # Set drawing mode to freedraw
@@ -25,23 +25,23 @@ canvas_result = st_canvas(
     stroke_color=stroke_color,
     background_color=bg_color,
     update_streamlit=realtime_update,
-    height=250,
-    width=250,
+    height=300,
+    width= 300,
     drawing_mode=drawing_mode,
     key="canvas",
 )
 
-# Add a "Recognize" button with styling
-if st.button("Recognize", key="recognize_button", help="Click to recognize the digit"):
+# Add a "Recognize" button
+if st.button("Recognize"):
     # Convert the canvas image to NumPy array
     image_array = np.array(canvas_result.image_data)
 
     # Preprocess the image for the model
-    img_array = np.asarray(Image.fromarray(image_array).convert("L").resize((28, 28))).reshape(1, 28 * 28) / 255.0
+    img_array = np.asarray(Image.fromarray(image_array).convert("L").resize((28, 28))).reshape(1, 28 , 28,1) / 255.0
 
     # Make prediction
     prediction = model.predict(img_array)
     predicted_class = np.argmax(prediction)
 
-    # Display the prediction with improved formatting
-    st.subheader(f"Prediction Result: **{predicted_class}**")
+    # Display the prediction
+    st.write(f"Prediction: {predicted_class}", font=("arial", 72, "Italic"))
